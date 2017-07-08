4977c546c4840f6a765b0e842afc3be43c6ed650
marco <git@marco-rebhan.de>
2017-07-08 12:53:55 +0200

    Initial commit

---
+++ src/main/kotlin/therealfarfetchd/quacklib/QuackLib.kt
@@ -0,0 +1,12 @@
+package therealfarfetchd.quacklib
+
+import net.minecraftforge.fml.common.Mod
+
+/**
+ * Created by marco on 08.07.17.
+ */
+
+const val ModID = "quacklib"
+
+@Mod(modid = ModID, modLanguageAdapter = "net.shadowfacts.forgelin.KotlinAdapter")
+object QuackLib
