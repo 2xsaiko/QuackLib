663829e2612171db8ce54805196cadafe943a0b1
the_real_farfetchd <gitlab@marco-rebhan.de>
2017-07-12 14:13:32 +0200

    add sound for wrench

--- src/main/kotlin/therealfarfetchd/quacklib/common/item/Wrench.kt
+++ src/main/kotlin/therealfarfetchd/quacklib/common/item/Wrench.kt
@@ -2,11 +2,9 @@ package therealfarfetchd.quacklib.common.item
 
 import net.minecraft.creativetab.CreativeTabs
 import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.init.SoundEvents
 import net.minecraft.item.Item
-import net.minecraft.util.EnumActionResult
-import net.minecraft.util.EnumFacing
-import net.minecraft.util.EnumHand
-import net.minecraft.util.ResourceLocation
+import net.minecraft.util.*
 import net.minecraft.util.math.BlockPos
 import net.minecraft.world.World
 import therealfarfetchd.quacklib.ModID
@@ -34,6 +32,7 @@ object Wrench : Item() {
         else block.rotateBlock(world, pos, facing)
 
     if (result) {
+      world.playSound(null, pos, SoundEvents.ITEM_ARMOR_EQUIP_IRON, SoundCategory.BLOCKS, 1.0f, 1.5f)
       return EnumActionResult.SUCCESS
     }
     return EnumActionResult.FAIL
